8d3f250fb6680dd7cf60a20e54a372637a52ff4f
miyapub <miyapub@outlook.com>
2017-08-21 00:33:49 +0800

    up

--- app/src/main/java/org/zhudou/api/timelogskotlin/LogsActivity.kt
+++ app/src/main/java/org/zhudou/api/timelogskotlin/LogsActivity.kt
@@ -1,10 +1,17 @@
 package org.zhudou.api.timelogskotlin
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.AdapterView
+import android.widget.DatePicker
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_logs.*
+import kotlinx.android.synthetic.main.timelog.*
+import kotlinx.android.synthetic.main.timelog.view.*
 
 class LogsActivity : AppCompatActivity() {
 
@@ -24,7 +31,7 @@ class LogsActivity : AppCompatActivity() {
                 val end_time = c.getString(c.getColumnIndex("end_time"))
 
                 val map = HashMap<String, Any>()
-                map.put("id", id);
+                map.put("log_id", id);
                 map.put("title", title);
                 map.put("status", status);
                 map.put("begin_time", begin_time);
@@ -34,12 +41,31 @@ class LogsActivity : AppCompatActivity() {
             }
         }
         var simpleAdapter = MyAdapter(baseContext, logs,
-                R.layout.timelog, arrayOf("id","title","begin_time","end_time"), intArrayOf(R.id.id,R.id.title,R.id.begin,R.id.end))
+                R.layout.timelog, arrayOf("log_id","title","begin_time","end_time"), intArrayOf(R.id.log_id,R.id.title,R.id.begin,R.id.end))
         time_logs.adapter=simpleAdapter
         add.setOnClickListener(View.OnClickListener {
             dbHelper.exec("insert into logs(title,begin_time,end_time,status)values('title',datetime('now','localtime'),datetime('now','localtime'),'doing')")
             val intent = Intent(this@LogsActivity, TimeActivity::class.java)
             startActivity(intent)
         })
+
+        time_logs.onItemLongClickListener= AdapterView.OnItemLongClickListener { adapterView, view, i, l ->
+            var id=view.log_id.text
+            var title_value=view.title.text
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            val alertDialog = alertDialogBuilder.create()
+            var input_title=EditText(this)
+            input_title.setText(title_value)
+            alertDialog.setView(input_title)
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"确定", DialogInterface.OnClickListener({ dialogInterface: DialogInterface, i: Int ->
+                var str_title=input_title.text
+                dbHelper.exec("update logs set title='$str_title' where _id='$id'")
+                view.title.setText(str_title)
+                simpleAdapter.notifyDataSetChanged()
+            }))
+            alertDialog.show()
+            return@OnItemLongClickListener true
+        }
+
     }
 }
